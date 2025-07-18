c626adf47756044be06397292cf9b400def76643
André <andre_miguel_costa@hotmail.com>
2025-07-18 18:25:47 +0200

    build: add cmakelists and toolchain cmake files

---
+++ cmake_toolchain_files/aarch64-none-linux-gnu.cmake
@@ -0,0 +1,6 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_C_COMPILER aarch64-none-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER aarch64-none-linux-gnu-gcc)
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O3 -ggdb")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -ggdb")
---
+++ cmake_toolchain_files/arm-none-linux-gnueabihf.cmake
@@ -0,0 +1,6 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_C_COMPILER arm-none-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER arm-none-linux-gnueabihf-g++)
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=armv7-a -mfpu=neon -O3 -ggdb")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=armv7-a -mfpu=neon -O3 -ggdb")
